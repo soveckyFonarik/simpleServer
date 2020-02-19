26ca5041fd3ac146aed79f3a014b75b4b8e175f0
Погореловский Михаил Александрович <dubov_ea@surgutneftegas.ru>
2020-02-19 15:25:58 +0500

    Add more cool stuff: custom error checking and api

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -4,4 +4,11 @@ import {
     handleBodyRequestParsing
 } from './common'
 
-export default [handleCors,handleBodyRequestParsing, handleCompression]
+import { handleAPIDocs } from './apiDocs'
+
+export default [
+    handleCors,
+    handleBodyRequestParsing,
+    handleCompression,
+    handleAPIDocs
+];
--- src/services/search/routes.ts
+++ src/services/search/routes.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from 'express';
-import { getPlacesByName } from './SearchController'
+import { getPlacesByName } from './SearchController';
+import {checkSearchParams} from '../../middleware/checks';
+
 export default [
     {
         path: '/api/v1/search',
         method: 'get',
         handler: [
-            async ({query}: Request, res: Response) => {
+            checkSearchParams,
+            async ({ query }: Request, res: Response) => {
                 const result = await getPlacesByName(query.q);
                 res.status(200).send(result);
             }
